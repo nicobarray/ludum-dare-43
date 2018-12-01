0591de0886fad9a686bbf4b29e405f6dcbc23c5b
Nicolas Barray <nbarray@gmail.com>
2018-12-01 11:57:11 +0100

    Add dices

---
+++ Assets/Csharp/Dices.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dices : MonoBehaviour
+{
+
+    public GameObject dicePrefab;
+
+    public List<Dice> dices;
+
+    public void AddDice()
+    {
+        GameObject newDice = Instantiate(
+            dicePrefab,
+            Vector3.zero,
+            Quaternion.AngleAxis(UnityEngine.Random.value * 360, Vector3.forward)
+        );
+        dices.Add(newDice.GetComponent<Dice>());
+        dices[dices.Count - 1].Shuffle();
+		ReorderDices();
+    }
+
+    void ReorderDices()
+    {
+        for (int i = 0; i < dices.Count; i++)
+        {
+            dices[i].transform.position = new Vector3(transform.position.x - i * Mathf.Sqrt(2), 0, 0);
+        }
+    }
+
+    public void RemoveDice(int index)
+    {
+		dices.RemoveAt(index);
+		ReorderDices();
+    }
+
+    public void Shuffle()
+    {
+        dices.ForEach(dice => dice.Shuffle());
+    }
+
+	void Start() {
+		ReorderDices();
+	}
+}
