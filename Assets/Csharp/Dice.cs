43463b317ca995c28af07c39b7525bd6261d1992
Nicolas Barray <nbarray@gmail.com>
2018-12-01 11:44:26 +0100

    Throw random dice results

---
+++ Assets/Csharp/Dice.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dice : MonoBehaviour {
+
+	public Sprite[] faces;
+
+	public SpriteRenderer spriteRenderer;
+
+	public int value = 1;
+
+	void RefreshSpriteRenderer() {
+		spriteRenderer.sprite = faces[value - 1];
+	}
+
+	public void Shuffle() {
+		value = UnityEngine.Random.Range(1, 7);
+		RefreshSpriteRenderer();
+	}
+
+	// Use this for initialization
+	void Start () {
+		RefreshSpriteRenderer();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.Space)) {
+			Shuffle();
+		}
+	}
+}
